d251fd9ac6e858165fbb1ee4352f82588171a2f6
Giuseppe Iacono <giuseiacono10@gmail.com>
2022-12-09 16:56:55 +0100

    Iacono esercizio Input and Output 04

---
+++ src/it/develhope/inputOutput04/PrintYourNameAndPi.java
@@ -0,0 +1,10 @@
+package it.develhope.inputOutput04;
+
+public class PrintYourNameAndPi {
+
+    public static void main(String[] args) {
+
+        System.out.printf("%S%n", "giuseppe");
+        System.out.printf("The number Pi is %.6f", Math.PI);
+    }
+}
